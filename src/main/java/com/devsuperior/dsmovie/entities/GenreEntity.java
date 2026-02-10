7e3350c8a485faadb21ef1bc2117091b26ca7d98
DyhegoS <dyhegodasilva@gmail.com>
2026-02-10 12:10:04 -0300

    versionamento URI

---
+++ src/main/java/com/devsuperior/dsmovie/entities/GenreEntity.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_genre")
+public class GenreEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "genre")
+	private List<MovieEntity> movies = new ArrayList<>();
+	
+	public GenreEntity() {
+	}
+
+	public GenreEntity(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	public List<MovieEntity> getMovies() {
+		return movies;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GenreEntity other = (GenreEntity) obj;
+		return Objects.equals(id, other.id);
+	}
+}
